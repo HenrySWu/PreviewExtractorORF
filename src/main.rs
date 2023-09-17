96863d895079446f57a5b711f2752da097a98d4e
HenrySWu <47734817+HenrySWu@users.noreply.github.com>
2023-09-17 10:42:35 -0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+// uses the tokio::spawn to use multiple cores
+
+use std::path::Path;
+use std::fs;
+use std::process::Command;
+
+async fn exiftoolcall(command1: String, command2: String) {
+  println!("{}", command1);
+  Command::new("cmd").args(&["/C", &command1]).output().expect("failed");
+  let cmd = Command::new("cmd").args(&["/C", &command2]).output().expect("failed");
+  print!("{}", String::from_utf8_lossy(&cmd.stdout));
+}
+
+#[tokio::main]
+async fn main() {
+  if !Path::new("./RAW").exists() {
+    println!("RAW foider does not exist");
+    let res = fs::create_dir_all("./RAW");
+    match res {
+      Ok(()) => println!("RAW folder created, place RAWs in this folder"),
+      Err(e) => println!("Error: {e:?}"),
+    }
+    std::process::exit(0);
+  } else {
+    if !Path::new("./RAW").is_dir() {
+      println!("There is a file named RAW in the folder, needs to be removed");
+    }
+  }
+  let res = fs::create_dir_all("./JPEG");
+  match res {
+    Ok(()) => (),
+    Err(e) => println!("Error: {e:?}"),
+  }
+
+  let p = fs::read_dir("./RAW/").unwrap();
+  for file in p {
+    let file = file.unwrap().path();
+    if !file.is_dir() {
+      let mut string1: String = "exiftool.exe -b -PreviewImage ".to_string();
+      let mut string2: String = "exiftool.exe -overwrite_original -TagsFromFile ".to_string();
+      string1.push_str(&file.display().to_string());
+      string1.push_str(" > JPEG/");
+      string1.push_str(&file.display().to_string()[6..file.display().to_string().len()-3]);
+      string1.push_str("jpg");
+      string2.push_str(&file.display().to_string());
+      string2.push_str(" -exif:all JPEG/");
+      string2.push_str(&file.display().to_string()[6..file.display().to_string().len()-3]);
+      string2.push_str("jpg");
+      tokio::spawn(async move {exiftoolcall(string1, string2).await;});
+    }
+  }
+}
